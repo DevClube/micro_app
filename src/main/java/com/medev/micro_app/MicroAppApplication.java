257a2476cf7dea6160d53b9445b1cb9e13ce6a21
Mehdi Mediani <mediani.mehdi.dev@gmail.com>
2024-02-10 16:40:31 +0100

    entities

---
+++ src/main/java/com/medev/micro_app/MicroAppApplication.java
@@ -0,0 +1,13 @@
+package com.medev.micro_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroAppApplication.class, args);
+    }
+
+}
